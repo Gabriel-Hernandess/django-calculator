b9cb505292a7fb8fa2a8dfd9ea160dcefd789905
Gabriel Yanagawa Hernandes <gabriel.hernandess01@gmail.com>
2025-07-24 02:09:18 -0300

    Implementado funções da calculadora e de registros de operações.

--- calculator/urls.py
+++ calculator/urls.py
@@ -3,6 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('calculator/', calculator_page, name='calculator'),
+    path('novo-registro/', novo_registro, name='novo-registro'),
+    path('limpar-registros/', limpar_registros, name='limpar_registros'),
+
     path('login/', login_page, name='login'),
     path('cadastro/', cadastro, name='cadastro'),
     path('logout/', logout_user, name='logout'),
--- calculator/views.py
+++ calculator/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth import authenticate, login, logout
 from .decorators import login_required_custom
 from django.contrib import messages
 from django.db import IntegrityError
+from django.http import JsonResponse
+import json
 
 from django.http import HttpResponse
-from .models import Usuario
+from .models import Usuario, Operacao
 
 def login_page(request):
     if request.method == 'GET':
@@ -16,9 +18,6 @@ def login_page(request):
         email = request.POST.get('email')
         senha = request.POST.get('senha')
 
-        print(email, senha)
-        print(Usuario.objects.all())
-
         try:
             user = Usuario.objects.get(Email=email, Senha=senha)
             
@@ -27,7 +26,6 @@ def login_page(request):
 
             return redirect('calculator')
         except Usuario.DoesNotExist:
-            msg = 'Usuário ou senha inválidos'
             return render(request, 'login.html', {'msg': 'Email ou senha incorreto'})
         
 def cadastro(request):
@@ -60,4 +58,55 @@ def logout_user(request):
 
 @login_required_custom
 def calculator_page(request):
-    return render(request, 'calculator.html')
+    if request.method == 'GET':
+        usuario_id = request.session.get('usuario_id')
+
+        try:
+            user = Usuario.objects.get(IDUsuario=usuario_id)
+        except Usuario.DoesNotExist:
+            return redirect('login')
+
+        operacoes_qs = Operacao.objects.filter(Usuario=user).order_by('-DtInclusao')
+        operacoes = list(operacoes_qs.values('Parametros', 'Resultado', 'DtInclusao'))
+
+        return render(request, 'calculator.html', {'operacoes': operacoes})
+
+@login_required_custom
+def novo_registro(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+
+            print(data)
+
+            usuario_id = data.get('usuarioId')
+            if not usuario_id:
+                return JsonResponse({'error': 'Usuário não informado'}, status=400)
+            
+            try:
+                usuario = Usuario.objects.get(pk=usuario_id)
+            except Usuario.DoesNotExist:
+                return JsonResponse({'error': 'Usuário não encontrado'}, status=404)
+            
+            Operacao.objects.create(
+                Usuario=usuario,
+                Parametros=data['expressao'],
+                Resultado=data['resultado']
+            )
+
+            return JsonResponse({'success': True})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+    return JsonResponse({'error': 'Método não permitido'}, status=405)
+
+@login_required_custom
+def limpar_registros(request):
+    if request.method == 'POST':
+        usuario_id = request.session.get('usuario_id')
+        if not usuario_id:
+            return JsonResponse({'error': 'Usuário não autenticado'}, status=401)
+
+        Operacao.objects.filter(Usuario_id=usuario_id).delete()
+        return JsonResponse({'success': True})
+
+    return JsonResponse({'error': 'Método não permitido'}, status=405)
